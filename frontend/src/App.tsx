f7e19dc28d27ecdacf2493b0ac7c7a0ab69c7c46
shugami <shubhambhardva341@gmail.com>
2024-09-19 22:12:29 +0530

    multilogin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,37 @@
 import './App.css'
 import Login from './auth/login'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import MainLayout from './Mainlayout'
+import Signup from './auth/Signup'
+
+const appRouter= createBrowserRouter([
+{
+path:"/",
+element:<MainLayout/>,
+// children:[{
+//   path:"/login"
+// }]
+
+},
+{
+  path:"/login",
+  element:<Login/>
+},
+{
+  path:"/signup",
+  element:<Signup/>
+},
+
+
+])
+
 function App() {
 return (
-  <>
- <Login/>
-</>
+  <main>
+    <RouterProvider router={appRouter}>
+
+    </RouterProvider>
+</main>
 )
 }
 export default App
