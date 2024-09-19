bfba574763e381bf65b11be2d749f89c1a6bb91c
DholariyaRikki <128449546+DholariyaRikki@users.noreply.github.com>
2024-09-19 22:42:44 +0530

    edit

---
+++ backend/models/user.models.ts
@@ -0,0 +1,81 @@
+import mongoose,{Document} from "mongoose";
+
+export interface IUser{
+    fullname:string
+    email:string
+    password:string
+    contact:string
+    address:string
+    city:string
+    country:string
+    profilepicture:string
+    admin:boolean
+    lastlogin?:Date
+    isverified?:boolean
+    resetpasswordtoken?:string
+    resetpasswordtokenexpire?:Date
+    verificationtoken?:string
+    verificationtokenexpireat?:Date
+}
+
+// export interface
+export interface IUserDocument extends IUser, Document{
+    createdAt:Date,
+    updatedAt:Date
+}
+
+// user schema
+const userSchema = new mongoose.Schema<IUserDocument>({
+    fullname:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    contact:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:String,
+        default:"update your address"
+    },
+    city:{
+        type:String,
+        default:"update your city"
+    },
+    country:{
+        type:String,
+        default:"update your country"
+    },
+    profilepicture:{
+        type:String,
+        default:""
+    },
+    admin:{
+        type:Boolean,
+        default:false
+    },
+    lastlogin:{
+        type:Date,
+        default:Date.now
+    },
+    isverified:{
+        type:Boolean,
+        default:false
+    },
+    resetpasswordtoken:String,
+    resetpasswordtokenexpire:Date,
+    verificationtoken:String,
+    verificationtokenexpireat:Date
+
+},{timestamps:true})
+
+// user model
+export const User = mongoose.model("User",userSchema)
