6efb4702b7f956f89f8480b3df834b7d252dda05
DholariyaRikki <128449546+DholariyaRikki@users.noreply.github.com>
2024-09-21 18:14:50 +0530

    edit

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./db/connectDB";
+import connectDB from "./db/connectdb";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin: "https://food-app-yt.onrender.com",
+    origin: "http://localhost:5173",
     credentials: true
 }
 app.use(cors(corsOptions));
@@ -35,9 +35,9 @@ app.use("/api/v1/restaurant", restaurantRoute);
 app.use("/api/v1/menu", menuRoute);
 app.use("/api/v1/order", orderRoute);
 
-app.use(express.static(path.join(DIRNAME,"/client/dist")));
+app.use(express.static(path.join(__dirname, '../frontend')));
 app.use("*",(_,res) => {
-    res.sendFile(path.resolve(DIRNAME, "client","dist","index.html"));
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
 app.listen(PORT, () => {
