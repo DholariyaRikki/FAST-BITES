91e4157b06fe56a86d33643a9b2c6633426c7c5f
DholariyaRikki <128449546+DholariyaRikki@users.noreply.github.com>
2024-09-21 17:29:02 +0530

    edit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -44,10 +44,11 @@ element:<Navbar/>,
 function App() {
 return (
   <main>
+        <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden"/>
+
     <RouterProvider router={appRouter}>
       
     </RouterProvider>
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden"/>
 </main>
 )
 }
